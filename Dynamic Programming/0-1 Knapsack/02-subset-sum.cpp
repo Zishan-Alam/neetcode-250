a8dbb8ca3f2daccd45dbefa2de2920535342ad90
Zishan Alam Khan <95163676+Zishan-Alam@users.noreply.github.com>
2025-12-31 18:21:43 +0530

    Add subset sum solution in 02-subset-sum.cpp
    Implement subset sum problem using dynamic programming.

---
+++ Dynamic Programming/0-1 Knapsack/02-subset-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool SubsetSum(vector<int>& nums, int sum) {
+        int n = nums.size();
+
+        vector<vector<bool>> t(n+1, vector<bool>(sum+1));
+
+        //Initializing 
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<sum+1; j++) {
+                if(i == 0) t[i][j] = false;
+                if(j == 0) t[i][j] = true;
+            }
+        }
+
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<sum+1; j++) {
+                if(nums[i-1] <= j) {
+                    t[i][j] = t[i-1][j - nums[i-1]] || t[i-1][j];
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
+};
+
+// Time: O(n * sum)
+// Space: O(n * sum)
